087ee4467c79a893d54276747a916a27f0298673
widvisser <wi.d.visser@st.hanze.nl>
2024-10-04 19:57:59 +0200

    Added first unit-test and basic functionality of function: getTwoByTwoContingencyTable and related functions. * Added test class `TableTest.java` * Added first ideal-case test (print option) * Implemented Table.*

--- src/main/java/nl/bioinf/dgsea/table_outputs/Table.java
+++ src/main/java/nl/bioinf/dgsea/table_outputs/Table.java
@@ -11,24 +11,57 @@ public abstract class Table {
     public static List<Pathway> pathways;
     public static List<PathwayGene> pathwayGenes;
 
-    public String getTwoByTwoContingencyTable() throws Exception {
-        throw new Exception("Not implemented yet");
+    public static void getTwoByTwoContingencyTable(TableOutputOptions tableOutputOption, String[] selectedPathways) {
+        int sumInPathway = Table.getSumInPathway(selectedPathways[0]);
+        int sumTotalPathway = Table.getSumTotalPathway(); // all non-and-significant degs
+        int sumOutPathway = sumTotalPathway - sumInPathway;
+        int sumIsSignificantDeg = getSumIsSignificantDeg();
+        int sumTotalDeg = getSumTotalDeg(); // all not- and- in pathway
+        int sumIsNotSigDeg = sumTotalDeg - sumIsSignificantDeg;
+        int ratioInPathway;
+        int ratioOutPathway;
+        if(sumOutPathway != 0) {ratioInPathway =  sumInPathway / sumOutPathway;} else { ratioInPathway = 1;}
+        if(sumInPathway != 0) {ratioOutPathway =  sumOutPathway / sumInPathway;} else { ratioOutPathway = 1;}
+        String result = """
+Notch singnaling pathway (%s)
+     | D | D* | Sum
+   C | %s | %s  | %s
+  C* | %s | %s  | %s
+ Sum | %s | %s  | %s
+
+ C: in pathway, C*: not in pathway
+ D: DEG (FDR <= 0.01), D*: non DEG""".formatted(
+                selectedPathways[0],
+                sumIsSignificantDeg*ratioInPathway, sumIsNotSigDeg*ratioInPathway, sumInPathway,
+                sumIsSignificantDeg*ratioOutPathway, sumIsNotSigDeg*ratioOutPathway, sumOutPathway,
+                sumIsSignificantDeg, sumIsNotSigDeg, sumTotalDeg
+
+        );
+        if (tableOutputOption == TableOutputOptions.PRINT) {
+            System.out.println(result);
+        }
     }
 
-    private int getSumInPathway() throws Exception {
-        throw new Exception("Not implemented yet");
+    private static int getSumInPathway(String pathway) {
+        int count = 0;
+        for (PathwayGene gene : pathwayGenes) {
+            if (gene.pathwayId().equals(pathway)) {
+                count++;
+            }
+        }
+        return count;
     }
 
-    private int getSumTotalPathway() throws Exception {
-        throw new Exception("Not implemented yet");
+    private static int getSumTotalPathway() {
+        return pathwayGenes.size();
     }
 
-    private int getSumIsSignificantDeg() throws Exception {
-        throw new Exception("Not implemented yet");
+    private static int getSumIsSignificantDeg() {
+        return (int) degs.stream().filter(deg -> deg.adjustedPValue() < 0.01).count();
     }
 
-    private int getSumTotalDeg() throws Exception {
-        throw new Exception("Not implemented yet");
+    private static int getSumTotalDeg() {
+        return degs.size();
     }
 }
 
---
+++ src/main/java/nl/bioinf/dgsea/table_outputs/TableOutputOptions.java
@@ -0,0 +1,5 @@
+package nl.bioinf.dgsea.table_outputs;
+
+public enum TableOutputOptions {
+    CSV,PRINT
+}
