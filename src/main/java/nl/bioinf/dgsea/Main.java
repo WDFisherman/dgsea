2b52d386bd3dda28ad296078b6ddd00d8a3348f4
widvisser <wi.d.visser@st.hanze.nl>
2024-10-08 09:37:49 +0200

    Other changes to: * gradle.xml * misc.xml * cleaned Main.java, since it's no longer going to contain actual running code

--- src/main/java/nl/bioinf/dgsea/Main.java
+++ src/main/java/nl/bioinf/dgsea/Main.java
@@ -1,19 +1,13 @@
 package nl.bioinf.dgsea;
 
 import picocli.CommandLine;
-
 import java.util.Arrays;
-import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
         int exitCode;
+        System.out.println("args = " + Arrays.toString(args));
         exitCode = new CommandLine(new CommandlineController()).execute(args);
         System.exit(exitCode);
-        Main main = new Main();
-        main.start(args);
-    }
-    public void start(String[] args) {
-        System.out.println("args.toString() = " + Arrays.toString(args));
     }
 }
